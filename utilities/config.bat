6508e6182d3a0d60c0989239e18ffd5dcaca2f50
RegularSpark <71980947+RegularSpark@users.noreply.github.com>
2021-06-22 16:18:14 -0800

    config.bat not being ignored part 97474598469869845

--- utilities/config.bat
+++ utilities/config.bat
@@ -3,16 +3,16 @@ if "%SUBSCRIPT%"=="" ( start notepad.exe "%CD%\%~nx0" & exit )
 endlocal
 
 :: verbose
-set VERBOSEWRAPPER=y
+set VERBOSEWRAPPER=n
 
 :: skip check depends
-set SKIPCHECKDEPENDS=y
+set SKIPCHECKDEPENDS=n
 
 :: skip depend install
 set SKIPDEPENDINSTALL=n
 
 :: dev mode
-set DEVMODE=y
+set DEVMODE=n
 
 :: dry run
 set DRYRUN=n
